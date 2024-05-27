322c2ba193ee35f8aeb76937ab0715e2c0fe8417
Jerry Qi <jerryq0101@gmail.com>
2024-05-27 12:08:35 -0700

    Scraper script into Mongo is successfully running

--- scripts/test_connection.js
+++ scripts/test_connection.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 import data from "../misc/items_list.json" assert { type: 'json' };
-import { MongoClient, ServerApiVersion } from "mongodb"
-const uri = process.env.MONGO_CONNECTION_STRING
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = process.env.MONGO_CONNECTION_STRING;
+const apikey = process.env.STEAM_API_KEY;
+const steamApiUrl = "https://api.steamapis.com/market/item/730/"
 
 // Connect operation to MongoDB, should return a necessary object of needed elements
 async function setup() {
@@ -12,32 +14,13 @@ async function setup() {
 
     await client.connect();
 
-    const dbName = "sample_mflix"
-    const collectionName = "comments"
+    const dbName = "items-data";
+    const collectionName = "properties";
 
     // Create references to the database and collection in order to run
     // operations on them.
     const database = client.db(dbName);
     const collection = database.collection(collectionName);
-
-    try {
-        await collection.insertOne(
-            {
-                name: "elotes",
-                ingredients: [
-                  "corn",
-                  "mayonnaise",
-                  "cotija cheese",
-                  "sour cream",
-                  "lime",
-                ],
-                prepTimeInMinutes: 35,
-            }
-        );
-        console.log("Sample data successfuly inserted into the collection");
-    } catch (err) {
-        console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
-    }
 
     return {
         collection,
@@ -45,12 +28,76 @@ async function setup() {
     };
 }
 
-  async function query(itemName) {
+// Write data to MongoDB
+async function writeMongoDB(data, collection, client) {
+    // Insert one specific item to the collection
+    await collection.insertOne(data)
+    console.log(`Successfully inserted ${data} into the collection`)
+}
+
+function convertEnglishToDate(englishDate) {
+    const dateArr = englishDate.split(" ");
+
+    if (dateArr.length != 3) {
+        return "ERROR, date is not in the correct format";
+    }
+
+    let month = 0;
+    if (dateArr[0] == "Jan") {
+        month = 1;
+    } else if (dateArr[0] == "Feb") {
+        month = 2;
+    } else if (dateArr[0] == "Mar") {
+        month = 3;
+    } else if (dateArr[0] == "Apr") {
+        month = 4;
+    } else if (dateArr[0] == "May") {
+        month = 5;
+    } else if (dateArr[0] == "Jun") {
+        month = 6;
+    } else if (dateArr[0] == "Jul") {
+        month = 7;
+    } else if (dateArr[0] == "Aug") {
+        month = 8;
+    } else if (dateArr[0] == "Sep") {
+        month = 9;
+    } else if (dateArr[0] == "Oct") {
+        month = 10;
+    } else if (dateArr[0] == "Nov") {
+        month = 11;
+    } else if (dateArr[0] == "Dec") {
+        month = 12;
+    }
+
+    return `${dateArr[2]}-${month}-${dateArr[1]}`;
+}
+
+  async function queryPrice(itemName) {
     // Your implementation here...
-    
+    // GET Request to SteamAPI
+    const response = await fetch(steamApiUrl + itemName + `?api_key=${apikey}` + `&median_history_days=10000`);
+    const responseJson = await response.json();
+    console.log("Item Name:", responseJson.market_hash_name)
+    console.log("Item Price History:", responseJson.median_avg_prices_10000days)
+
+    let itemPriceHistoryObjFormArr = []
+    // Reformat the array to be more readable
+    for (const instance of responseJson.median_avg_prices_10000days) {
+        // Process the date
+        let dateFormatted = convertEnglishToDate(instance[0]);
+        const medianPrice = instance[1];
+        const volume = instance[2];
+        itemPriceHistoryObjFormArr.push({
+            date: dateFormatted,
+            median_price: medianPrice,
+            volume: volume
+        });
+    }
+
+    return itemPriceHistoryObjFormArr;
   }
   
-  // Create a delay function
+  // A delay function (for api limits)
   function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
@@ -63,47 +110,53 @@ async function setup() {
     const itemsWithProperties = data.data;
     const itemsStrings = itemsWithProperties.map(item => item.market_hash_name);
 
-    async function writeMongoDB(data) {
-        // Your implementation here...
-        
-    }
-
-    
     // Setup Mongo
     const {collection, client} = await setup();
 
+    // // Test process 
+    // console.log("Begin process")
+    // const itemName = "AK-47 | Redline (Field-Tested)"
+    // const stepOne = await queryPrice(itemName);
+    // console.log(stepOne)
+    // const stepTwo = await writeMongoDB({
+    //     name: itemName,
+    //     price_history: stepOne
+    // }, collection, client)
+    // console.log("End Process", stepTwo)
 
-
-    // // Loop through each existing csgo item
-    // for (const item of itemsStrings) {
-    //   if (minuteRequests >= 100) {
-    //     // If we've hit the rate limit for the minute, wait until the next minute
-    //     await delay(60000);
-    //     minuteRequests = 0;
-    //   }
+    // Loop through each existing csgo item and add it to the collection
+    for (const item of itemsStrings) {
+        console.log("Processing item:", item)
+      if (minuteRequests >= 100) {
+        // If we've hit the rate limit for the minute, wait until the next minute
+        await delay(60000);
+        minuteRequests = 0;
+      }
     
-    //   if (dailyRequests >= 5000) {
-    //     // If we've hit the rate limit for the day, wait until the next day
-    //     await delay(86400000);
-    //     dailyRequests = 0;
-    //   }
+      if (dailyRequests >= 5000) {
+        // If we've hit the rate limit for the day, wait until the next day
+        await delay(86400000);
+        dailyRequests = 0;
+      }
     
-    //   // Within minute and daily limits, fetch data for the item and store it.
-    //   try {
-    //     const res = await query(item);
-    //     const data = await res.json();
-    
-    //       await writeMongoDB(data);
-    //       minuteRequests++;
-    //       dailyRequests++;
+      // Within minute and daily limits, fetch data for the item and store it.
+      try {
+        const data = await queryPrice(item);
+        await writeMongoDB({
+            name: item,
+            price_history: data
+        }, collection, client);
+        minuteRequests++;
+        dailyRequests++;
+        console.log("Successfully fetched data for item:", item)
         
-    //     } catch (error) {
-    //         console.error(`Failed to fetch data for item: ${item}`, error);
-    //     }
-    // }
+        } catch (error) {
+            console.error(`Failed to fetch data for item: ${item}`, error);
+        }
+    }
     
     
-    // Not sure why client.close() is not working inside of another function
+    // Why running the program was returning an client closed error:
     // Sol'n: Because the linear execution process of non await lines,
     // when calling collection.insertOne(), the operation is a process that isn't finished yet
     // thus causing the client.close() to be called before the insertOne() operation is finished
