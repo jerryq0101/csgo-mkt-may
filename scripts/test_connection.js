06b454966ce8a460c7f739152a19ee0161a31f1b
Jerry Qi <jerryq0101@gmail.com>
2024-05-28 16:28:37 -0700

    Added safe guard try catches in all sync functions

--- scripts/test_connection.js
+++ scripts/test_connection.js
@@ -22,34 +22,112 @@ async function setup() {
     const database = client.db(dbName);
     const collection = database.collection(collectionName);
 
+    if (collection == null || client == null) {
+        throw new Error("Failed to setup MongoDB");
+    }
+
     return {
         collection,
         client
     };
 }
 
-// Write data to MongoDB
-async function writeMongoDB(data, collection, client) {
-    // Insert one specific item to the collection
-    await collection.insertOne(data)
-    console.log(`Successfully inserted ${data} into the collection`)
+// MongoDB Operations
+async function insertMongoDB(data, collection, client) {
+    try {
+        // Insert one specific item to the collection
+        await collection.insertOne(data)
+        console.log(`Successfully inserted ${data} into the collection`)
+    } catch (error) {
+        throw new Error(`Failed to insert data into the collection: ${error}`);
+    }
 }
 
 async function updatePricesMongoDB(itemName, collection, client) {
     console.log("Updating prices for item:", itemName);
     const replacementPrices = await queryPrice(itemName);
-    await collection.replaceOne({ name: itemName }, {
-        name: itemName,
-        price_history: replacementPrices
-    });
-    console.log("Successfully updated prices for item:", itemName);
+    if (Number.isInteger(replacementPrices)) {
+        throw new Error("Failed to fetch data, automatically returning")
+    }
+    try {
+        await collection.replaceOne({ name: itemName }, {
+            name: itemName,
+            price_history: replacementPrices
+        });
+        console.log("Successfully updated prices for item:", itemName);
+    } catch (error) {
+        throw new Error(`Failed to update prices for item (Mongo Replace): ${itemName}`, error);
+    }
 }
 
+async function existsInCollection(itemName, collection) {
+    const query = { name: itemName };
+    try {
+        const result = await collection.findOne(query);
+        return result;
+    } catch (error) {
+        throw new Error(`Failed to find item in collection: ${error}`);
+    }
+}
+
+let minuteRequests = 0;
+let dailyRequests = 0;
+
+async function queryPrice(itemName) {
+    // Handle API Limits
+    if (minuteRequests >= 100) {
+        // If we've hit the rate limit for the minute, wait until the next minute
+        await delay(60000);
+        minuteRequests = 0;
+    }
+
+    // For clean request starts
+    if (dailyRequests >= 5000) {
+        // If we've hit the rate limit for the day, wait until the next day
+        await delay(86400000);
+        dailyRequests = 0;
+    }
+
+    // GET Request to SteamAPI
+    const response = await fetch(steamApiUrl + itemName + `?api_key=${apikey}` + `&median_history_days=10000`);
+
+    // For unclean request starts
+    if (response.status == 429) {
+        console.log(response)
+        await delay(86400000);
+        dailyRequests = 0;
+    } else if (response.status != 200) {
+        throw new Error("Failed to fetch data, automatically returning, not 200 or 429", response)
+    } else {
+        minuteRequests++;
+        dailyRequests++;
+    }
+
+    const responseJson = await response.json();
+
+    // Reformat the array to be more readable
+    let itemPriceHistoryObjFormArr = []
+    for (const instance of responseJson.median_avg_prices_10000days) {
+        // Process the date
+        let dateFormatted = convertEnglishToDate(instance[0]);
+        const medianPrice = instance[1];
+        const volume = instance[2];
+        itemPriceHistoryObjFormArr.push({
+            date: dateFormatted,
+            median_price: medianPrice,
+            volume: volume
+        });
+    }
+
+    return itemPriceHistoryObjFormArr;
+}
+
+// Pure Utility Functions
 function convertEnglishToDate(englishDate) {
     const dateArr = englishDate.split(" ");
 
     if (dateArr.length != 3) {
-        return "ERROR, date is not in the correct format";
+        throw new Error("Invalid date format")
     }
 
     let month = 0;
@@ -82,67 +160,6 @@ function convertEnglishToDate(englishDate) {
     return `${dateArr[2]}-${month}-${dateArr[1]}`;
 }
 
-let minuteRequests = 0;
-let dailyRequests = 0;
-
-async function queryPrice(itemName) {
-    // Handle API Limits
-    if (minuteRequests >= 100) {
-        // If we've hit the rate limit for the minute, wait until the next minute
-        await delay(60000);
-        minuteRequests = 0;
-    }
-
-    // For clean request starts
-    if (dailyRequests >= 5000) {
-        // If we've hit the rate limit for the day, wait until the next day
-        await delay(86400000);
-        dailyRequests = 0;
-    }
-
-    // GET Request to SteamAPI
-    const response = await fetch(steamApiUrl + itemName + `?api_key=${apikey}` + `&median_history_days=10000`);
-
-    // For unclean request starts
-    if (response.status == 429) {
-        console.log(response)
-        await delay(86400000);
-        dailyRequests = 0;
-    } 
-    
-    if (response.status != 200) {
-        console.log(response)
-        return response.status;
-    } else {
-        minuteRequests++;
-        dailyRequests++;
-    }
-
-    const responseJson = await response.json();
-
-    // Reformat the array to be more readable
-    let itemPriceHistoryObjFormArr = []
-    for (const instance of responseJson.median_avg_prices_10000days) {
-        // Process the date
-        let dateFormatted = convertEnglishToDate(instance[0]);
-        const medianPrice = instance[1];
-        const volume = instance[2];
-        itemPriceHistoryObjFormArr.push({
-            date: dateFormatted,
-            median_price: medianPrice,
-            volume: volume
-        });
-    }
-
-    return itemPriceHistoryObjFormArr;
-}
-
-async function existsInCollection(itemName, collection) {
-    const query = { name: itemName };
-    const result = await collection.findOne(query);
-    return result;
-}
-
 function isLatestPrices(data) {
     // check for like 4 days behind (long term investors only)
     const lastDay = data[data.length - 1];
@@ -154,12 +171,23 @@ function isLatestPrices(data) {
     return today - date <= 345600000;
 }
 
-// A delay function (for api limits)
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// This function should only be called once
+
+// fetchData()
+// Objective: 
+// to populate priceData for each possible csgo item, 
+// and if the item already exists in the collection, check if the prices 
+// are within the last 4 days, if not, update the prices.
+// Guidelines: 
+// Failure to fetch data for one item stops the entire script, whether the item already exists in the collection or not.
+    // A response code other than 429 returns the status code (Caught using Number.isInteger() checks)
+    // queryPrice() function handles 429 responses by waiting until the next day to fetch data
+// Failure for MongoDB operations are not yet handled
+// This script should be run once every week (Allowing for all items to be fetched in API rate limits, and prices to not be too behind)
+
 async function fetchData() {
     const itemsWithProperties = data.data;
     const itemsStrings = itemsWithProperties.map(item => item.market_hash_name);
@@ -167,8 +195,8 @@ async function fetchData() {
     // Setup Mongo
     const { collection, client } = await setup();
 
-    // // Test process 
-    // console.log("Begin process")r
+    // // Single Write Operation to MongoDB 
+    // console.log("Begin process")
     // const itemName = "AK-47 | Redline (Field-Tested)"
     // const stepOne = await queryPrice(itemName);
     // console.log(stepOne)
@@ -182,15 +210,17 @@ async function fetchData() {
     for (const item of itemsStrings) {
         // Check if the item has already been added to the collection
         const exists = await existsInCollection(item, collection)
-
+        
         if (exists) {
             // Check if the item has the latest date prices
             if (!isLatestPrices(exists.price_history)) {
                 // If the item doesn't have the latest prices, update the prices
                 await updatePricesMongoDB(item, collection, client);
+                console.log("Updated prices for item:", item)
+            } else {
+                console.log("Item prices are the latest in collection:", item);
+                continue;
             }
-            console.log("Item already exists in collection:", item);
-            continue;
         }
         console.log("Processing item:", item)
 
@@ -198,21 +228,14 @@ async function fetchData() {
         try {
             const data = await queryPrice(item);
             
-            // If Error
-            if (Number.isInteger(data)) {
-                console.log("Failed to fetch data, automatically returning")
-                return;
-            }
-
-            await writeMongoDB({
+            await insertMongoDB({
                 name: item,
                 price_history: data
             }, collection, client);
-
-            console.log("Successfully fetched data for item:", item)
-
+            console.log("Successfully fetched data for a new item:", item)
         } catch (error) {
-            console.error(`Failed to fetch data for item: ${item}`, error);
+            console.error(`Failed to fetch data for the new item: ${item}`, error);
+            return;
         }
     }
 
