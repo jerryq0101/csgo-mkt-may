d0aabffbc25eee7ab41dad0298d7c4836bbb50bc
Jerry Qi <jerryq0101@gmail.com>
2024-06-04 19:05:28 -0700

    update note

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import { GlobalContextComponent } from "@/app/GlobalContext.js";
 export default async function Home() {
     return (
       <div>
-        <div className="flex justify-center items-center min-h-screen py-5 lg:px-10 md:px-5 sm:px-0 bg-zinc-700 sm:overflow-auto">
+        <div className="flex justify-center items-center min-h-screen py-10 lg:px-10 md:px-5 sm:px-0 bg-zinc-700 sm:overflow-auto">
               <GlobalContextComponent>
-                <div className="flex flex-col gap-5">
+                <div className="mt-10 flex flex-col gap-5">
                   <div className="flex h-fit xl:flex-row sm:flex-col sm:items-left gap-7">
                     <Search />
                     <Chart />
