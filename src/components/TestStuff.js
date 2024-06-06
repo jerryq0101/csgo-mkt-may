718c4e4be42aa90d6e1ec89d0abf465d160cb610
Jerry Qi <jerryq0101@gmail.com>
2024-06-06 16:29:02 -0700

    Layout stacking works

--- src/components/TestStuff.js
+++ src/components/TestStuff.js
@@ -6,30 +6,33 @@ import GridLayout, {Responsive, WidthProvider} from "react-grid-layout";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default function TestStuff() {
-    const layout1 = [
-        { i: "a", x: 0, y: 0, w: 1, h: 2 },
-        { i: "b", x: 1, y: 0, w: 1, h: 2,},
-        { i: "c", x: 4, y: 0, w: 1, h: 2 },
-    ]
-    const layout2 = [
-        { i: "a", x: 0, y: 0, w: 1, h: 2 },
-        { i: "b", x: 1, y: 0, w: 1, h: 2 },
-        { i: "c", x: 2, y: 0, w: 1, h: 2 },
-    ]
+    const layout1 = {}
 
     return (
         <>
-
             <ResponsiveGridLayout
-                className="layout react-grid-layout overflow-none"
-                layouts={[layout1, layout2]}
-                breakpoints={{ lg: 1024, md: 768, sm: 640, xs: 480, xxs: 250}}
-                cols={{ lg: 10, md: 8, sm: 4, xs: 2, xxs: 1}}
+                className="layout bg-red-300"
+                cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+                rowHeight={30}
+                layouts={layout1}
             >
-                <div className="bg-black react-grid-item select-none" key="a">a</div>
-                <div className="bg-black react-grid-item select-none" key="b">b</div>
-                <div className="bg-black react-grid-item select-none" key="c">c</div>
+                <div className="bg-green-300"key="1" data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}>
+                    <span className="text">1</span>
+                </div>
+                <div className="bg-green-300"key="2" data-grid={{ w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3 }}>
+                    <span className="text">2</span>
+                </div>
+                <div className="bg-green-300"key="3" data-grid={{ w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3 }}>
+                    <span className="text">3</span>
+                </div>
+                <div className="bg-green-300"key="4" data-grid={{ w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}>
+                    <span className="text">4</span>
+                </div>
+                <div className="bg-green-300"key="5" data-grid={{ w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }}>
+                    <span className="text">5</span>
+                </div>
             </ResponsiveGridLayout>
+            
         </>
     )
 }
