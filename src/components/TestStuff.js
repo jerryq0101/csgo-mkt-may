8f414d34bfba5e490f3d03aa79f34d479a3355e8
Jerry Qi <jerryq0101@gmail.com>
2024-06-07 11:58:55 -0700

    Implement Draggable handle and default responsive for search and chart

--- src/components/TestStuff.js
+++ src/components/TestStuff.js
@@ -12,33 +12,29 @@ export default function TestStuff() {
     // (Called Second)
     useEffect(() => {
         console.log("first useeffect called")
-        console.log("layout state", layouts)
         const layout = JSON.parse(localStorage.getItem("layout"));
         if (layout) {
             setLayouts(layout);
         } else {
-            setLayouts({});
+            setLayouts(defaultLayout);
         }
     }, [])
 
-    // Update Layout for state and localStorage 
-    // (Called First)
-    const updateL = (layout, layouts) => {
-        console.log("UpdateL called");
-        console.log("Layout State", layouts)
-        setLayouts(layouts);
-        console.log("Layout", layout);
-        console.log("Layouts", layouts);
-    }
-
     // (Called Third, 1st render)
+    // Not called if layout did not change
     useEffect(() => {
         console.log("Second useeffect called")
-        console.log("Layout State", layouts)
         if (Object.keys(layouts).length > 0) {
             localStorage.setItem("layout", JSON.stringify(layouts));
         }
     }, [layouts])
+
+    // Update Layout for state and localStorage 
+    // (Called First, component mounting)
+    const updateL = (layout, layouts) => {
+        console.log("UpdateL called");
+        setLayouts(layouts);
+    }
 
     /*
     Documentation for any grid Item
@@ -69,48 +65,69 @@ export default function TestStuff() {
         isBounded: ?boolean = false
         }
     */
+    
+    // // Search = 1 col version
+    // const defaultLayout = {
+    //     "xxl": [
+    //         { i: "1", w: 1, h: 3, x: 0, y: 0, minW: 1, minH: 3 },
+    //         { i: "2", w: 2, h: 3, x: 3, y: 0, minW: 2, minH: 3 },
+    //         { i: "3", w: 1, h: 3, x: 5, y: 0, minW: 1, minH: 3 },
+    //     ],
+    //     "xl": [
+    //         { i: "1", w: 1, h: 3, x: 0, y: 0, minW: 1, minH: 3 },
+    //         { i: "2", w: 1, h: 3, x: 3, y: 0, minW: 1, minH: 3 },
+    //         { i: "3", w: 1, h: 3, x: 5, y: 0, minW: 1, minH: 3 },
+    //     ],
+    //     "lg": [
+    //         { i: "1", w: 1, h: 3, x: 0, y: 0, minW: 1, minH: 3 },
+    //         { i: "2", w: 1, h: 3, x: 3, y: 0, minW: 1, minH: 3 },
+    //         { i: "3", w: 2, h: 3, x: 5, y: 0, minW: 2, minH: 3 },
+    //     ],
+    //     "md": [
+    //         { i: "1", w: 1, h: 3, x: 0, y: 0, minW: 1, minH: 2 },
+    //         { i: "2", w: 1, h: 3, x: 3, y: 0, minW: 2, minH: 2 },
+    //         { i: "3", w: 2, h: 3, x: 5, y: 0, minW: 2, minH: 2 },
+    //     ],
+    //     "sm": [
+    //         { i: "1", w: 1, h: 3, x: 0, y: 0, minW: 1, minH: 2 },
+    //         { i: "2", w: 1, h: 3, x: 3, y: 0, minW: 1, minH: 2 },
+    //         { i: "3", w: 1, h: 2, x: 5, y: 0, minW: 1, minH: 2 },
+    //     ],
+    // }
+
+    // // Search = 2 col version
+    const defaultLayout = {
+        "xxl": [
+            { i: "1", w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 },
+            { i: "2", w: 4, h: 3, x: 3, y: 0, minW: 3, minH: 3 },
+            { i: "3", w: 1, h: 3, x: 5, y: 0, minW: 1, minH: 3 },
+        ],
+        "xl": [
+            { i: "1", w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 },
+            { i: "2", w: 3, h: 3, x: 3, y: 0, minW: 3, minH: 3 },
+            { i: "3", w: 1, h: 3, x: 5, y: 0, minW: 1, minH: 3 },
+        ],
+        "lg": [
+            { i: "1", w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 },
+            { i: "2", w: 3, h: 3, x: 3, y: 0, minW: 3, minH: 3 },
+            { i: "3", w: 2, h: 3, x: 5, y: 0, minW: 2, minH: 3 },
+        ],
+        "md": [
+            { i: "1", w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 },
+            { i: "2", w: 2, h: 3, x: 3, y: 0, minW: 2, minH: 3 },
+            { i: "3", w: 2, h: 3, x: 5, y: 0, minW: 2, minH: 3 },
+        ],
+        "sm": [
+            { i: "1", w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 },
+            { i: "2", w: 2, h: 3, x: 3, y: 0, minW: 2, minH: 3 },
+            { i: "3", w: 1, h: 2, x: 5, y: 0, minW: 1, minH: 3 },
+        ],
+    }
 
     // Steps:
-    // Test if the format is actually accurate YES IT WORKS IN THIS FORMAT
-    // Use the correct method to store the layout in localstorage
-
-    const testLayout = {
-        // "lg": [
-        //     { i: "1", w: 3, h: 3, x: 0, y: 0, minW: 3, minH: 3 },
-        //     { i: "2", w: 2, h: 3, x: 3, y: 0, minW: 2, minH: 3 },
-        //     { i: "3", w: 2, h: 3, x: 5, y: 0, minW: 2, minH: 3 },
-        //     { i: "4", w: 2, h: 3, x: 7, y: 0, minW: 2, minH: 3 },
-        //     { i: "5", w: 2, h: 3, x: 9, y: 0, minW: 2, minH: 3 }
-        // ],
-        // "md": [
-        //     { i: "1", w: 2, h: 2, x: Math.floor(Math.random() * 6), y: Math.floor(Math.random() * 6), minW: 2, minH: 2 },
-        //     { i: "2", w: 2, h: 2, x: Math.floor(Math.random() * 6), y: Math.floor(Math.random() * 6), minW: 2, minH: 2 },
-        //     { i: "3", w: 2, h: 2, x: Math.floor(Math.random() * 6), y: Math.floor(Math.random() * 6), minW: 2, minH: 2 },
-        //     { i: "4", w: 2, h: 2, x: Math.floor(Math.random() * 6), y: Math.floor(Math.random() * 6), minW: 2, minH: 2 },
-        //     { i: "5", w: 2, h: 2, x: Math.floor(Math.random() * 6), y: Math.floor(Math.random() * 6), minW: 2, minH: 2 }
-        // ],
-        // "sm": [
-        //     { i: "1", w: 1, h: 2, x: Math.floor(Math.random() * 5), y: Math.floor(Math.random() * 5), minW: 1, minH: 2 },
-        //     { i: "2", w: 1, h: 2, x: Math.floor(Math.random() * 5), y: Math.floor(Math.random() * 5), minW: 1, minH: 2 },
-        //     { i: "3", w: 1, h: 2, x: Math.floor(Math.random() * 5), y: Math.floor(Math.random() * 5), minW: 1, minH: 2 },
-        //     { i: "4", w: 1, h: 2, x: Math.floor(Math.random() * 5), y: Math.floor(Math.random() * 5), minW: 1, minH: 2 },
-        //     { i: "5", w: 1, h: 2, x: Math.floor(Math.random() * 5), y: Math.floor(Math.random() * 5), minW: 1, minH: 2 }
-        // ],
-        // "xs": [
-        //     { i: "1", w: 1, h: 1, x: Math.floor(Math.random() * 4), y: Math.floor(Math.random() * 4), minW: 1, minH: 1 },
-        //     { i: "2", w: 1, h: 1, x: Math.floor(Math.random() * 4), y: Math.floor(Math.random() * 4), minW: 1, minH: 1 },
-        //     { i: "3", w: 1, h: 1, x: Math.floor(Math.random() * 4), y: Math.floor(Math.random() * 4), minW: 1, minH: 1 },
-        //     { i: "4", w: 1, h: 1, x: Math.floor(Math.random() * 4), y: Math.floor(Math.random() * 4), minW: 1, minH: 1 },
-        //     { i: "5", w: 1, h: 1, x: Math.floor(Math.random() * 4), y: Math.floor(Math.random() * 4), minW: 1, minH: 1 }
-        // ],
-        // "xxs": [
-        //     { i: "1", w: 1, h: 1, x: Math.floor(Math.random() * 2), y: Math.floor(Math.random() * 2), minW: 1, minH: 1 },
-        //     { i: "2", w: 1, h: 1, x: Math.floor(Math.random() * 2), y: Math.floor(Math.random() * 2), minW: 1, minH: 1 },
-        //     { i: "3", w: 1, h: 1, x: Math.floor(Math.random() * 2), y: Math.floor(Math.random() * 2), minW: 1, minH: 1 },
-        //     { i: "4", w: 1, h: 1, x: Math.floor(Math.random() * 2), y: Math.floor(Math.random() * 2), minW: 1, minH: 1 },
-        //     { i: "5", w: 1, h: 1, x: Math.floor(Math.random() * 2), y: Math.floor(Math.random() * 2), minW: 1, minH: 1 }
-        // ]
-    }
+    // Change breakpoints to tailwind ones DONE
+    // Change cols to my own DONE
+    // change default layout to have minW, width and height (maybe coordinates that work)
 
     return (
         <>
@@ -134,29 +151,34 @@ export default function TestStuff() {
             // e.g. {lg: Layout, md: Layout, ...}
             layouts: {[key: $Keys<breakpoints>]: Layout},
         */}
+
             <ResponsiveGridLayout
-                className="layout bg-red-300"
-                breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-                cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-                rowHeight={30}
+                className="layout bg-red-300 select-none"
+                breakpoints={{xxl: 1536, xl: 1280, lg: 1024, md: 768, sm: 480}}
+                // // search = 1 col Version
+                // cols={{ xxl: 3, xl: 2, lg: 2, md: 1, sm: 1 }}
+                // search = 2 col version
+                cols={{ xxl: 6, xl: 5, lg: 5, md: 2, sm: 2 }}
+                rowHeight={50}
+                margin={[10, 10]}
                 layouts={layouts}
                 onLayoutChange={updateL}
+                draggableHandle=".draggable-handle"
             >
-                <div className="bg-green-300"key="1" >
+                <div className="bg-green-300 flex"key="1" >
                     <span className="text">1</span>
+                    <span className="draggable-handle">
+                        <hr />
+                        [DRAG HERE!]
+                        <hr />
+                    </span>
                 </div>
                 <div className="bg-green-300"key="2" >
                     <span className="text">2</span>
                 </div>
-                <div className="bg-green-300"key="3" >
+                {/* <div className="bg-green-300"key="3" >
                     <span className="text">3</span>
-                </div>
-                <div className="bg-green-300"key="4" >
-                    <span className="text">4</span>
-                </div>
-                <div className="bg-green-300"key="5" >
-                    <span className="text">5</span>
-                </div>
+                </div> */}
             </ResponsiveGridLayout>
             
         </>
