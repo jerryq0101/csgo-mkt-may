fad9e1e951c6941c2af33d369d2e47278da5950a
Jerry Qi <jerryq0101@gmail.com>
2024-06-08 12:12:59 -0700

    Adding safeguards for disconnection.ts

--- src/app/lib/dbconnection.ts
+++ src/app/lib/dbconnection.ts
@@ -2,10 +2,6 @@ import { MongoClient, Db } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-if (!process.env.MONGO_CONNECTION_STRING) {
-    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
-
 export default class MongoDbConnection {
     private static instance: MongoDbConnection;
     private dbClient: MongoClient | null = null;
@@ -23,6 +19,9 @@ export default class MongoDbConnection {
 
     private async connect() {
         const uri = process.env.MONGO_CONNECTION_STRING;
+        if (!uri) {
+            throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+        }
         this.dbClient = new MongoClient(uri);
         await this.dbClient.connect();
         this.db = this.dbClient.db("items-data");
